2802d908ac1b23bb39128319422ef6597ec4b91b
Bruno Mendes <brod56@sapo.pt>
2021-02-19 07:48:34 +0000

    Ignore cmake build files

--- tp1-brute-force-greedy-algorithms/cal_fp01_CLion/cmake-build-debug/lib/googletest-master/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bdmendes/Documents/Faculdade/2ano-2S/CAL/feup-cal/tp1-brute-force-greedy-algorithms/cal_fp01_CLion/lib/googletest-master/googletest
-# Build directory: /home/bdmendes/Documents/Faculdade/2ano-2S/CAL/feup-cal/tp1-brute-force-greedy-algorithms/cal_fp01_CLion/cmake-build-debug/lib/googletest-master/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
