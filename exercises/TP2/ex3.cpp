f655cb6e55ddd490e49e9278f9dd9867a900cc8c
Bruno Mendes <brod56@sapo.pt>
2021-02-26 21:55:31 +0000

    Add tp2 ex3

--- exercises/TP2/ex3.cpp
+++ exercises/TP2/ex3.cpp
@@ -2,28 +2,28 @@
 
 bool changeMakingBacktrackingRec(unsigned int C[], unsigned int Stock[], unsigned int n, unsigned int T,
                                  unsigned curIndex, unsigned &curBestNumberOfCoins, unsigned curCoins[], unsigned bestCoins[]){
-    if (curIndex >= n - 1){
-        return T == 0;
+    // Base cases
+    if (T == 0){
+        unsigned used = 0;
+        for (unsigned i = 0; i < n; ++i){
+            used += curCoins[i];
+        }
+        if (used < curBestNumberOfCoins){
+            for (unsigned i = 0; i < n; ++i) bestCoins[i] = curCoins[i];
+            curBestNumberOfCoins = used;
+        }
+        return true;
+    }
+    else if (curIndex == n){
+        return false;
     }
 
     // Try to add one more coin
     bool foundWithCoin = false;
-    if (curCoins[curIndex] < Stock[curIndex]){
+    if (curCoins[curIndex] < Stock[curIndex] && C[curIndex] <= T){
         curCoins[curIndex]++;
         foundWithCoin = changeMakingBacktrackingRec(C,Stock,n,T - C[curIndex], curIndex, curBestNumberOfCoins, curCoins, bestCoins);
-
-        // Check if found solution is the best yet
-        if (foundWithCoin){
-            unsigned used = 0;
-            for (unsigned i = 0; i < n; ++i) used += curCoins[i];
-            if (used < curBestNumberOfCoins){
-                for (unsigned i = 0; i < n; ++i) bestCoins[i] = curCoins[i];
-                curBestNumberOfCoins = used;
-            }
-        }
-
-        // Reset curCoins for the next call
-        curCoins[curIndex]--;
+        curCoins[curIndex]--; // reset curCoins for the next coin trial
     }
 
     // Try to switch to the next coin
@@ -44,6 +44,17 @@ bool changeMakingBacktracking(unsigned int C[], unsigned int Stock[], unsigned i
 
 /// TESTS ///
 #include <gtest/gtest.h>
+
+TEST(TP2_Ex3, simpleTest){
+    unsigned int C[] = {1,2,3};
+    unsigned int Stock[] = {1,2,1};
+    unsigned int n = 3;
+    unsigned int usedCoins[3];
+    EXPECT_EQ(changeMakingBacktracking(C,Stock,n,5,usedCoins), true);
+    EXPECT_EQ(usedCoins[0], 0);
+    EXPECT_EQ(usedCoins[1], 1);
+    EXPECT_EQ(usedCoins[2], 1);
+}
 
 TEST(TP2_Ex3, hasBFChangeCanonical) {
     unsigned int C[] = {1,2,5,10};
