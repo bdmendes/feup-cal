921a4cf29039088e2af5b075ee317bc9046ff624
Bruno Mendes <brod56@sapo.pt>
2021-04-07 18:19:35 +0100

    Remove build artifacts

--- build/lib/googletest-master/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bdmendes/Documents/Faculdade/2ano-2S/CAL/feup-cal/lib/googletest-master
-# Build directory: /home/bdmendes/Documents/Faculdade/2ano-2S/CAL/feup-cal/build/lib/googletest-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- build/lib/googletest-master/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bdmendes/Documents/Faculdade/2ano-2S/CAL/feup-cal/lib/googletest-master/googletest
-# Build directory: /home/bdmendes/Documents/Faculdade/2ano-2S/CAL/feup-cal/build/lib/googletest-master/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
