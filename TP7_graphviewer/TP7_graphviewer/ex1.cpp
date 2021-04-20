750ce4068fd919a8b38ba5ea1bd731d2b1dd42bf
Bruno Mendes <brod56@sapo.pt>
2021-04-20 14:13:47 +0100

    Add tp8 stubs

---
+++ TP7_graphviewer/TP7_graphviewer/ex1.cpp
@@ -0,0 +1,9 @@
+#include "graphviewer.h"
+
+using namespace std;
+using Node = GraphViewer::Node;
+using Edge = GraphViewer::Edge;
+
+void ex1() {
+    // TODO
+}
---
+++ TP7_graphviewer/TP7_graphviewer/ex2.cpp
@@ -0,0 +1,10 @@
+#include "graphviewer.h"
+
+using namespace std;
+using Node = GraphViewer::Node;
+using Edge = GraphViewer::Edge;
+
+void ex2() {
+    // TODO
+}
+
